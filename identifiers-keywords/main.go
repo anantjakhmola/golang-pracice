522f4800ad5d170fc86b6583943053266196ec66
anant jakhmola <anantjakhmola9@mail.com>
2020-07-02 21:09:53 +0530

    push 1

---
+++ identifiers-keywords/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 44 //assign and declare
+	fmt.Println(x)
+	x = 99 //only assign
+	fmt.Println(x)
+	y := 100 + 70 //expression
+	fmt.Println(y)
+	z := "We work in dark to serve the light we are Assassins"
+	fmt.Println(z)
+}
